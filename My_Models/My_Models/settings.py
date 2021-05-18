90fbe4fe8ba5bf3fb8bac0eabd1b9421779aded1
aurfkwar <81349933+aurfkwar@users.noreply.github.com>
2021-05-18 18:36:50 -0500

    Update settings.py
    Deleted Secret Key

--- My_Models/My_Models/settings.py
+++ My_Models/My_Models/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ep8^)$7=1ye)j%q1+0cj#-zjk_2jtd=@$d0qy=6m7+n4bf9h@)'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
